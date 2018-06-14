f373ddb4dfd4405765ee192c96d203c04868607b
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-13 19:43:44 -0700

    booking, reviews, info working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const request = require('request');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(/(\/rooms\/100|\/rooms\/[1-9][0-9]|\/rooms\/[1-9])\b/, express.static(path.join(__dirname, '../public')));
-app.use('*/node_modules', express.static(path.join(__dirname, '../node_modules')));
+
+app.get(/\/api\/rooms\/\d+\/(general|amenities)/, (req, res) => {
+  const forwardURL = `http://ec2-18-217-35-168.us-east-2.compute.amazonaws.com${req.url}`;
+  req.pipe(request(forwardURL)).pipe(res);
+});
+app.get(/\/api\/listings\/\d+\/(averagestars|reviews)/, (req, res) => {
+  const forwardURL = `http://localhost:3004${req.url}`;
+  req.pipe(request(forwardURL)).pipe(res);
+});
+app.get('/api/rooms/:listingID/bookingInfo', (req, res) => {
+  const forwardURL = `http://localhost:3002${req.url}`;
+  req.pipe(request(forwardURL)).pipe(res);
+});
+
 
 app.listen(3000);
