fda16584831abd5d1c058c6c2b0d6f23a5f83d98
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-17 18:42:01 -0700

    deployed versions of modules

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const request = require('request');
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(/(\/rooms\/100|\/rooms\/[1-9][0-9]|\/rooms\/[1-9])\b/, express.static(path.join(__dirname, '../public')));
+app.use('/rooms/:id', express.static(path.join(__dirname, '../public')));
 
 app.get(/\/api\/rooms\/\d+\/(general|amenities)/, (req, res) => {
   const forwardURL = `http://ec2-18-217-35-168.us-east-2.compute.amazonaws.com${req.url}`;
-  req.pipe(request(forwardURL)).pipe(res);
+  res.redirect(forwardURL);
 });
 app.get(/\/api\/listings\/\d+\/(averagestars|reviews)/, (req, res) => {
-  const forwardURL = `http://localhost:3004${req.url}`;
-  req.pipe(request(forwardURL)).pipe(res);
+  const forwardURL = `http://ec2-54-241-139-163.us-west-1.compute.amazonaws.com${req.url}`;
+  res.redirect(forwardURL);
 });
 app.get('/api/rooms/:listingID/bookingInfo', (req, res) => {
-  const forwardURL = `http://localhost:3002${req.url}`;
-  req.pipe(request(forwardURL)).pipe(res);
+  const forwardURL = `http://ec2-34-210-45-213.us-west-2.compute.amazonaws.com${req.url}`;
+  res.redirect(forwardURL);
 });
-
+app.post('/rooms', (req, res) => {
+  res.redirect(307, 'http://localhost:3001/rooms');
+});
 
 app.listen(3000);
